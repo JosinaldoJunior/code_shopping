a3f0456f0a358ec0184fefe6efeece4c0f5d6461
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-06-11 10:04:25 -0300

    Criado/atualizado recurso de usuaários.

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,36 +6,34 @@ use CodeShopping\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Builder;
 use CodeShopping\User;
+use CodeShopping\Http\Resources\UserResource;
+use CodeShopping\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
     public function index(Request $request)
     {
         $users = User::paginate(10);
-        return $users;
+        return UserResource::collection($users);
     }
 
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        $user = User::createCustom($request->all());
+        return new UserResource($user);
     }
 
     public function show(User $user)
     {
-        return $user;
+        return new UserResource($user);
     }
 
-    public function update(Request $request, User $user)
+    public function update(UserRequest $request, User $user)
     {
         //
     }
 
     public function destroy(User $user)
-    {
-        //
-    }
-    
-    private function onlyTrashedIfRequested(Request $request, Builder $query)
     {
         //
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public static function createCustom($attributes = array())
+    {
+        !isset($attributes['password']) ? : $attributes['password'] = bcrypt($attributes['password']);
+        return parent::create($attributes);
+    }
 }
