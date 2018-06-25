c8d8fa09cdbb6c3aeea2b9450d64c6be6d27dfa4
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-06-25 13:27:12 -0300

    Criado componente de Login.

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
