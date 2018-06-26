27ef557417922e3d50e8c1d88c5abb435df49ef5
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-06-26 10:10:49 -0300

    Criando a primeira requisição ajax para a API.

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/pages/login/login.component';
+import { FormsModule } from '@angular/Forms';
+import { HttpClientModule } from '@angular/common/http';
 
+//ES7 - Java Script
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- _angular-app/src/app/components/pages/login/login.component.ts
+++ _angular-app/src/app/components/pages/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  credentials = {
+     email : '',
+     password: ''
+  };
+  
+  //###################################### INFORMAÇÕES IMPORTANTES ########################################
+  //property biding -> [innerHTML]
+  //Símbolo [] - O TS(Type Script) reflete alterações no template | Dados alteram ---> Template
+  //Símbolo () - O Evento reflete alterações no TS(Type Script) | Template alteram ---> Dados
+  //Two way data biding
+  
+  constructor(private http: HttpClient) { //injeção de dependência automática
+      
+  }
 
   ngOnInit() {
+      
+  }
+  
+  submit(){
+      //Enviar uma requisição ajax com as credenciais para API
+      this.http.post('http://localhost:8000/api/login', this.credentials)
+               .subscribe((data) => console.log(data));
+      
+      return false;
   }
 
 }
