69881c518a901df02b1dada314edae30da78ec17
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-07-03 14:01:49 -0300

    Criando cadastro de categorias.

--- _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ _angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+declare let $;
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -10,12 +12,35 @@ export class CategoryListComponent implements OnInit {
     
   categories: Array<{id: number, name: string, active: boolean, created_at: {date: string}}> = [];
 
+  category = {
+          name: ''
+  };
+  
   constructor(private http:HttpClient) { 
 //      console.log('construtor');
   }
 
   ngOnInit() {
       console.log('ngOnInit');
+      this.getCategories();
+  }
+  
+  submit(){
+      const token = window.localStorage.getItem('token');// Pega o token da API.
+      this.http
+          .post('http://localhost:8000/api/categories', this.category, {
+              headers: {
+                  'Authorization' : `Bearer ${token}`
+              }
+          })
+          .subscribe((category) => {
+              console.log(category);
+              this.getCategories();
+              $('#exampleModal').modal('hide');
+          });
+  }
+  
+  getCategories(){
       const token = window.localStorage.getItem('token');// Pega o token da API.
       this.http.get<{data: Array<{id: number, name: string, active: boolean, created_at: {date: string}}>}>
       ('http://localhost:8000/api/categories', {
