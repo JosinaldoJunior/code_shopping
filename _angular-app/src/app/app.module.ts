a95a2f491cb88cfc22e6f337e165b327e149775a
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-06 14:59:10 -0300

    Componente para listar os erros de um campo.

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -32,6 +32,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SortColumnComponent } from './components/common/sort-column/sort-column.component';
 import { CategorySearchFormComponent } from './components/pages/category/category-search-form/category-search-form.component';
 import { CategoryFormComponent } from './components/pages/category/category-form/category-form.component';
+import { FieldErrorComponent } from './components/bootstrap/field-error/field-error.component';
 
 function jwtFactory(authService: AuthService){
     return {
@@ -70,7 +71,8 @@ function jwtFactory(authService: AuthService){
     NavbarComponent,
     SortColumnComponent,
     CategorySearchFormComponent,
-    CategoryFormComponent
+    CategoryFormComponent,
+    FieldErrorComponent
   ],
   imports: [
     BrowserModule,
