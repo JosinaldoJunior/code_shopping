a4456dd875f7b0145af854be52fdb69bba0e63eb
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-06 15:24:05 -0300

    Listando erros de validação.

---
+++ _angular-app/src/app/common/validation-message.ts
@@ -0,0 +1,16 @@
+const messages = {
+    required: ':name é requerido.',
+    minlength: ':name precisa ter no mínimo :min caracteres.',
+    maxlength: ':name precisa ter no máximo :min caracteres.',
+    email: ':name não é um e-mail válido.'
+}
+
+export class ValidationMessage{
+    static getMessage(error: string, replaceTokens: Array<any>){
+        let message = messages[error];
+        const tokens = message.match(/\:[a-z]+/g);
+        tokens.forEach((token, index) => message = message.replace(token, replaceTokens[index]));
+        
+        return message;
+    }
+}
--- _angular-app/src/app/components/bootstrap/field-error/field-error.component.ts
+++ _angular-app/src/app/components/bootstrap/field-error/field-error.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ValidationMessage } from '../../../../app/common/validation-message';
 
 @Component({
   selector: 'field-error',
@@ -18,5 +19,22 @@ export class FieldErrorComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  get errorKeys(){
+      return Object.keys(this.errors);
+  }
+  
+  get errors(){
+      return this.field.errors;
+  }
+  
+  showError(){
+      return this.field.invalid && (this.field.dirty || this.field.touched);
+  }
+  
+  getMessage(error){
+      return ValidationMessage.getMessage(error, ['label']);
+  }
+  
 
 }
