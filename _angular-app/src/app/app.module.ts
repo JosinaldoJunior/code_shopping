83dee442ef3144987a94a830848d661214a4db80
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-09 13:33:50 -0300

    Alterando IsInvalid

--- _angular-app/src/app/app.module.ts
+++ _angular-app/src/app/app.module.ts
@@ -33,7 +33,7 @@ import { SortColumnComponent } from './components/common/sort-column/sort-column
 import { CategorySearchFormComponent } from './components/pages/category/category-search-form/category-search-form.component';
 import { CategoryFormComponent } from './components/pages/category/category-form/category-form.component';
 import { FieldErrorComponent } from './components/bootstrap/field-error/field-error.component';
-import { IsInvalidDirective } from './directives/is-invalid.directive';
+import { IsInvalidDirective, IsInvalidControlDirective } from './directives/is-invalid.directive';
 import { ListErrorComponent } from './components/bootstrap/list-error/list-error.component';
 import { CardErrorComponent } from './components/bootstrap/card-error/card-error.component';
 import { ProductInputListComponent } from './components/pages/product-input/product-input-list/product-input-list.component';
@@ -83,6 +83,7 @@ function jwtFactory(authService: AuthService){
     CategoryFormComponent,
     FieldErrorComponent,
     IsInvalidDirective,
+    IsInvalidControlDirective,
     ListErrorComponent,
     CardErrorComponent,
     ProductInputListComponent,
