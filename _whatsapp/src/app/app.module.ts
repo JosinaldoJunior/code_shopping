f96a98db1fe5c4134df9c295b21f9539a03a88b4
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-22 09:50:41 -0300

    Definindo navegação para opção de login

--- _whatsapp/src/app/app.module.ts
+++ _whatsapp/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginOptionsPage } from '../pages/login-options/login-options';
+import { LoginPhoneNumberPage } from '../pages/login-phone-number/login-phone-number';
+import { ResetPhoneNumberPage } from '../pages/reset-phone-number/reset-phone-number';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +16,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginOptionsPage,
+    LoginPhoneNumberPage,
+    ResetPhoneNumberPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +29,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginOptionsPage,
+    LoginPhoneNumberPage,
+    ResetPhoneNumberPage
   ],
   providers: [
     StatusBar,
