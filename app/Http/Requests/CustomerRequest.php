e6186afbb7ab9126f4a5db40350f397c96a738c6
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-30 12:57:18 -0300

    Validação dos dados do cliente.

--- app/Http/Requests/CustomerRequest.php
+++ app/Http/Requests/CustomerRequest.php
@@ -3,6 +3,8 @@
 namespace CodeShopping\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use CodeShopping\Rules\FirebaseTokenVerification;
+use CodeShopping\Rules\PhoneNumberUnique;
 
 class CustomerRequest extends FormRequest
 {
@@ -24,9 +26,14 @@ class CustomerRequest extends FormRequest
     public function rules()
     {
         return [
-//             'name' => 'required|max:255',
-//             'email' => 'required|max:255|email|unique:users,email',
-//             'password' => 'required|min:4|max:16'
+            'name' => 'required|max:255',
+            'email' => 'required|email|unique:users,email',
+            'photo' => 'image|max:' . (3 * 2014),
+            'token' => [
+                'required',
+                new FirebaseTokenVerification(),
+                new PhoneNumberUnique()
+            ]
         ];
     }
 }
