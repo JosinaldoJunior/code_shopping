64cf801b8ef8736f623c73adf96a152faf0fc699
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-31 14:01:44 -0300

    Interface para remover foto do usuário

--- _angular-app/src/app/components/pages/user-profile/user-profile.component.ts
+++ _angular-app/src/app/components/pages/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, AbstractControl} from '@angular/forms';
 import { NotifyMessageService } from '../../../services/notify-message.service';
 import { UserProfileHttpService } from '../../../services/http/user-profile-http.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'user-profile',
@@ -12,10 +13,12 @@ export class UserProfileComponent implements OnInit {
 
   form: FormGroup;
   errors = {};
+  has_photo: boolean;
   
   constructor(private formBuilder: FormBuilder,
               private userProfileHttp: UserProfileHttpService,
-              private notifyMessage: NotifyMessageService) { 
+              private notifyMessage: NotifyMessageService,
+              private authService: AuthService) { 
       this.form = this.formBuilder.group({
          name: ['', [Validators.maxLength(255)]],
          email: ['', [Validators.email, Validators.maxLength(255)]],
@@ -23,14 +26,21 @@ export class UserProfileComponent implements OnInit {
          phone_number: null,
          photo: false
       });
+      
+      this.form.patchValue(this.authService.me);
+      this.form.get('phone_number').setValue(this.authService.me.profile.phone_number);
+      this.has_photo = this.authService.me.profile.has_photo;
   }
 
   ngOnInit() {
   }
   
   submit(){
+      const data = Object.assign({}, this.form.value);
+      delete data.phone_number;
+      
       this.userProfileHttp
-          .update(this.form.value)
+          .update(data)
           .subscribe(
               (data) => this.notifyMessage.success('Perfil atualizado com sucesso!'),
               (responseError) => {
@@ -48,6 +58,11 @@ export class UserProfileComponent implements OnInit {
       }
       
       this.form.get('photo').setValue(files[0]);
+  }
+  
+  removePhoto(){
+      this.form.get('photo').setValue(null);
+      this.has_photo = false;
   }
   
   showErrors(){
