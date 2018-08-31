253b346970ee3cdcef4ba563b827237436d61f9d
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-08-31 14:52:41 -0300

    Integração do Firebase no Angular;

---
+++ _angular-app/src/app/firebase-config.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDT4hRROERi6BbO2cOTbS5dw5JEVgdjNDA",
+    authDomain: "codeshopping-4aa3a.firebaseapp.com",
+    databaseURL: "https://codeshopping-4aa3a.firebaseio.com",
+    projectId: "codeshopping-4aa3a",
+    storageBucket: "codeshopping-4aa3a.appspot.com",
+    messagingSenderId: "101309526098"
+};
