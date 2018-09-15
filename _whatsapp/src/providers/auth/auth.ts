27c585e8770bf2c87580e755b47f11e453e4b708
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-09-15 08:21:15 -0300

    Capturando Firebase uid do token JWT

--- _whatsapp/src/providers/auth/auth.ts
+++ _whatsapp/src/providers/auth/auth.ts
@@ -4,6 +4,10 @@ import { Observable } from 'rxjs/Observable'; //biblioteca para operações ass�
 import { FirebaseAuthProvider } from './firebase-auth';
 import { fromPromise } from 'rxjs/observable/fromPromise';
 import { flatMap } from 'rxjs/operators';
+import { User } from '../../app/model';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const TOKEN_KEY = 'code_shopping_token';
 
 /*
   Generated class for the AuthProvider provider.
@@ -14,8 +18,11 @@ import { flatMap } from 'rxjs/operators';
 @Injectable()
 export class AuthProvider {
 
+  me: User = null;
+
   constructor(public http: HttpClient, private firebaseAuth: FirebaseAuthProvider) {
-    //console.log('Hello AuthProvider Provider');
+      const token = this.getToken();
+      this.setUserFromToken(token);
   }
   
   login(): Observable<{token: string}>{
@@ -27,6 +34,30 @@ export class AuthProvider {
                  return this.http.post<{token: string}>('http://localhost:8000/api/login_vendor', {token})
              })
           );
+  }
+  
+  setToken(token: string){
+      this.setUserFromToken(token);
+      token ? window.localStorage.setItem(TOKEN_KEY, token) : window.localStorage.removeItem(TOKEN_KEY);
+  }
+  
+  private setUserFromToken(token: string){
+      const decodedToken =  new JwtHelperService().decodeToken(token);
+      this.me = decodedToken ? {
+          id: decodedToken.sub, 
+          name: decodedToken.name, 
+          email: decodedToken.email,
+          profile: decodedToken.profile
+      } : null;
+  }
+  
+  getToken() : string | null{
+      return window.localStorage.getItem(TOKEN_KEY);
+  }
+  
+  isAuth() : boolean{
+      const token = this.getToken();
+      return !new JwtHelperService().isTokenExpired(token, 30);
   }
 
 }
