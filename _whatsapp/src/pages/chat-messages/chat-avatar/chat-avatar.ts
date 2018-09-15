639df916a4c0eb0a1fe17474ebab64e7666c6f53
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-09-15 08:53:10 -0300

    Alinhamento das fotos

--- _whatsapp/src/pages/chat-messages/chat-avatar/chat-avatar.ts
+++ _whatsapp/src/pages/chat-messages/chat-avatar/chat-avatar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 /**
  * Generated class for the ChatAvatarComponent component.
@@ -12,11 +12,10 @@ import { Component } from '@angular/core';
 })
 export class ChatAvatarComponent {
 
-  text: string;
+  @Input()
+  position: string;
 
   constructor() {
-    console.log('Hello ChatAvatarComponent Component');
-    this.text = 'Hello World';
   }
 
 }
