7a9459ee817db91c85fe4da03ad4d51edfab357a
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-09-24 12:47:09 -0300

    Criando primeira mensagem pelo Laravel

--- app/Firebase/ChatMessageFb.php
+++ app/Firebase/ChatMessageFb.php
@@ -7,16 +7,34 @@ class ChatMessageFb
 {
     use FirebaseSync;
     
-    public function create(array $data) \\ chat_group, type, user, content
+    private $chatGroup;
+    
+    public function create(array $data) //chat_group, type, user, content
     {
+        $this->chatGroup =  $data['chat_group'];
         $type = $data['type'];
        
         switch($type){
             case 'audio':
             case 'image':
-            case 'text':
         }
+        
+        $reference = $this->getMessagesReference();
+        $reference->push([
+            'type'       => $data['type'],
+            'content'    => $data['content'],
+            'created_at' => ['.sv' => 'timestamp'],
+            'user_id'    => $data['firebase_uid']
+        ]);
         
         //um código para iniciar no firebase
     }
+    
+    private function getMessagesReference()
+    {
+        $path = "/chat_groups/{$this->chatGroup->id}/messages";
+        return $this->getFirebaseDatabase()->getReference($path);
+    }
+    
+    
 }
