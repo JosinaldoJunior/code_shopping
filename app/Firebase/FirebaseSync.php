2b0439725f177341accdff23d7f35674fd2c570c
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-10-13 09:15:20 -0300

    Definindo timestamps para created e updated_at.

--- app/Firebase/FirebaseSync.php
+++ app/Firebase/FirebaseSync.php
@@ -9,6 +9,9 @@ use Kreait\Firebase\Database\Reference;
 
 trait FirebaseSync 
 {
+    protected static $OPERATION_CREATE = 1;
+    protected static $OPERATION_UPDATE = 2;
+    
     public static function bootFirebaseSync()
     {
         static::created(function($model){
@@ -39,17 +42,34 @@ trait FirebaseSync
     
     protected function syncFbCreate()
     {
-        $this->syncFbSet();
+        $this->syncFbSet(self::$OPERATION_CREATE);
     }
     
     protected function syncFbUpdate()
     {
-        $this->syncFbSet();
+        $this->syncFbSet(self::$OPERATION_UPDATE);
     }
     
-    protected function syncFbSet()
+    protected function syncFbSet($operation = null)
     {
+        $data = $this->toArray();
+        $this->setTimestamps($data, $operation);
         $this->getModelReference()->update($this->toArray());
+    }
+    
+    protected function setTimestamps(&$data, $operation = null)
+    {
+        if($operation == self::$OPERATION_CREATE){
+            $data['created_at'] = ['.sv' => 'timestamp'];
+            $data['updated_at'] = ['.sv' => 'timestamp'];
+        }
+        
+        if($operation == self::$OPERATION_UPDATE){
+            if(isset($data['updated_at'])){
+                unset($data['updated_at']);
+            }
+            $data['updated_at'] = ['.sv' => 'timestamp'];
+        }
     }
     
     protected function syncFbRemove()
