fd8f7331b61e03b5f07b5f19f453e93c30069666
Josinaldo Junior <josenaldo.pedro@gmail.com>
2018-10-18 23:04:00 -0300

    determinando visto ou não visto para um grupo.

--- _whatsapp/src/providers/chat-group-viewer/chat-group-viewer.ts
+++ _whatsapp/src/providers/chat-group-viewer/chat-group-viewer.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ChatGroup } from '../../app/model';
+import { Observable } from 'rxjs/Observable';
+import { IsCurrentUserPipe } from '../../pipes/is-current-user/is-current-user';
 
 const CHAT_GROUPS_VIEWED_KEY = 'chat_groups_viewed';
 
@@ -13,17 +15,42 @@ const CHAT_GROUPS_VIEWED_KEY = 'chat_groups_viewed';
 @Injectable()
 export class ChatGroupViewerProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(private isCurrentUser: IsCurrentUserPipe) {
     console.log('Hello ChatGroupViewerProvider Provider');
   }
   
   loadViewed(group: ChatGroup){
-      this.setChatGroup(group);
+      this.hasViewed(group).subscribe((hasViewed) => {
+          group.viewed = hasViewed;
+          this.setChatGroup(group);
+      });
+  }
+  
+  private hasViewed(group: ChatGroup) : Observable<boolean>{
+      //não no localstorage - não visto
+      //ultima msg proprio user - visto
+      //data de atualização for diferente - negrito
+      return Observable.create(observer => {
+          let collection = this.collection;
+          
+          if(collection.hasOwnProperty(group.id) && collection[group.id].viewed === true){
+              group.last_message.subscribe(message => {
+                  const hasViewed = this.isCurrentUser.transform(message.user_id) || 
+                  group.updated_at === collection[group.id].updated_at;
+                  
+                  observer.next(hasViewed);
+              })
+              
+              return;
+          }
+          
+          observer.next(false); //não visto
+      });
   }
   
   private setChatGroup(group: ChatGroup){
       const collection = this.collection;
-      collection[group.id] = {name: group.name};
+      collection[group.id] = {viewed: group.viewed, updated_at: group.updated_at};
       
       this.collection = collection;
   }
